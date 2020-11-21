315b35fe933554107ee42af37ed8b01e017c230f
starwilly <willystar@gmail.com>
2020-11-21 15:00:21 +0800

    search github

---
+++ src/RepoCard.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let repo;
+</script>
+
+<style>
+    .repo-card {
+        border: 1px solid #ccc;
+        width: 300px;
+        border-radius: 8px;
+        padding: 0.5em;
+    }
+
+    .avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+
+</style>
+
+<div class="repo-card">
+{#if repo}
+    <img class="avatar" src={repo.owner.avatar_url} alt="owner">
+    <a href={repo.html_url} target="_blank">{repo.name}</a>
+    star: {repo.stargazers_count}
+{/if}
+</div>
+
